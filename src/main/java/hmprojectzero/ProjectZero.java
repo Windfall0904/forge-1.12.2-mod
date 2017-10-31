d21ce7b5efff9c0eac4b8857cbfc61270fee2ebb
Windfall0904 <31150043+Windfall0904@users.noreply.github.com>
2017-10-31 15:50:58 -0700

    main java modification. -preInit, init and postInit

--- src/main/java/hmprojectzero/ProjectZero.java
+++ src/main/java/hmprojectzero/ProjectZero.java
@@ -3,16 +3,37 @@ package hmprojectzero;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ProjectZero.MODID, version = ProjectZero.VERSION)
+
+
+@Mod(modid = ProjectZero.MODID, version = ProjectZero.VERSION,
+        name = ProjectZero.NAME, acceptedMinecraftVersions = "1.12.2")
 public class ProjectZero
 {
     public static final String MODID = "projectzero";
     public static final String VERSION = "0.0.1";
+    public static final String NAME = "Project Zero";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
     {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
